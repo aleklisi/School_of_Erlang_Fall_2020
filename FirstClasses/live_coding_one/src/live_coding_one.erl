a31ebe04d4d8dcdbaf0feab26ad2ea4f2dc4c9ca
Aleksander Lisiecki <sebaczechu123@gmail.com>
2020-10-28 13:05:05 +0100

    Add property base test to is_odd example

--- FirstClasses/live_coding_one/src/live_coding_one.erl
+++ FirstClasses/live_coding_one/src/live_coding_one.erl
@@ -1,3 +1,7 @@
 -module(live_coding_one).
 
--export([]).
+-export([is_odd/1]).
+
+is_odd(Number) ->
+    Number rem 2 == 1.
+
---
+++ FirstClasses/live_coding_one/test/live_coding_one_test.erl
@@ -0,0 +1,41 @@
+-module(live_coding_one_test).
+
+-include_lib("proper/include/proper.hrl").
+-include_lib("eunit/include/eunit.hrl").
+
+one_is_odd_test() ->
+    ?assert(live_coding_one:is_odd(1)).
+
+two_is_not_odd_test() ->
+    ?assertEqual(false, live_coding_one:is_odd(2)).
+
+more_odd_numbers_test() ->
+    ?assert(live_coding_one:is_odd(3)),
+    ?assert(live_coding_one:is_odd(7)),
+    ?assert(live_coding_one:is_odd(15)),
+    ?assert(live_coding_one:is_odd(21)).
+
+more_odd_not_numbers_test() ->
+    ?assert(false == live_coding_one:is_odd(4)),
+    ?assert(false == live_coding_one:is_odd(8)),
+    ?assert(false == live_coding_one:is_odd(16)),
+    ?assert(false == live_coding_one:is_odd(24)).
+
+
+exactly_one_odd_it_N_and_N_plus_one_pair(true, false) -> true;
+exactly_one_odd_it_N_and_N_plus_one_pair(false, true) -> true;
+exactly_one_odd_it_N_and_N_plus_one_pair(_, _) -> false.
+
+prop_exactly_one_odd_it_N_and_N_plus_one_pair() ->
+    ?FORALL(N,
+        non_neg_integer(),
+        exactly_one_odd_it_N_and_N_plus_one_pair(
+            live_coding_one:is_odd(N),
+            live_coding_one:is_odd(N+1)
+        )
+    ).
+
+exactly_one_odd_test() ->
+    ?assert(
+        proper:quickcheck(prop_exactly_one_odd_it_N_and_N_plus_one_pair(), 10000)
+    ).
