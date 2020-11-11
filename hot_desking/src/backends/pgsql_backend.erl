f6fd10b70220ec7fcc7625288c05a7ff02179f1d
Aleksander Lisiecki <sebaczechu123@gmail.com>
2020-11-11 09:13:34 +0100

    Provide a naive psql_worker implementation

--- hot_desking/src/backends/pgsql_backend.erl
+++ hot_desking/src/backends/pgsql_backend.erl
@@ -6,16 +6,30 @@
 -export([start_link/1]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2]).
 
-start_link(Name) ->
-    gen_server:start_link({local, Name}, ?MODULE, [], []).
+start_link(Args = #{name := Name}) ->
+    gen_server:start_link({local, Name}, ?MODULE, Args, []);
+start_link(Args) ->
+    gen_server:start_link(?MODULE, Args, []).
 
-init(Args) ->
-    State = Args,
-    {ok, State}.
+% TODO use Args to get to the connection
+init(_Args) ->
+    {ok, Conn} = epgsql:connect(#{
+        host => "localhost",
+        port => 5432,
+        username => "postgres",
+        password => "postgres",
+        database => "hot_desking",
+        timeout => 4000
+    }),
+    {ok, #{conn => Conn}}.
 
-handle_call(Call, _From, State) ->
-    logger:warning("Unexpected call ~p", [Call]),
-    {reply, Call, State}.
+% check https://github.com/epgsql/epgsql#simple-query
+handle_call(get_all_users, _From, State = #{conn := Conn}) ->
+    {ok, _, RawUsers} = epgsql:squery(Conn, "select username from users"),
+    Users = lists:map(fun({User}) -> User end, RawUsers),
+    {reply, Users, State};
+handle_call(_Request, _From, State) ->
+    {reply, ok, State}.
 
 handle_cast(Cast, State) ->
     logger:warning("Unexpected cast ~p", [Cast]),
