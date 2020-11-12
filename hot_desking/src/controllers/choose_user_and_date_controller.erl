ce4e5e991996d09cb368621204b4deb8e59c1d7d
Aleksander Lisiecki <sebaczechu123@gmail.com>
2020-11-12 13:09:53 +0100

    Use genserver to get data all users names

--- hot_desking/src/controllers/choose_user_and_date_controller.erl
+++ hot_desking/src/controllers/choose_user_and_date_controller.erl
@@ -5,6 +5,13 @@
 index(#{req := #{path := <<"/choose_user_and_date">>}}) ->
     {ok, []};
 index(#{req := #{path := <<"/all_users">>}}) ->
-    Response = [<<"Aleksander">>, <<"Rick">>, <<"Morty">>],
-    % TODO replace static Response list with the one taken from DB.
+    Response = maybe_start_db_and_get_all_the_users(),
     {json, Response}.
+
+% TODO handle starting and requesting data form pgsql correctly
+maybe_start_db_and_get_all_the_users() ->
+    DbPid = case pgsql_backend:start_link(db) of
+        {ok, Pid} -> Pid;
+        {error, {already_started, Pid}} -> Pid
+    end,
+    gen_server:call(DbPid, get_all_users).
