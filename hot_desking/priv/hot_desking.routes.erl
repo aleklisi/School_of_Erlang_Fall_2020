6f0a1170b5c34e0e95f5fab6684b38d2b8602a83
Aleksander Lisiecki <sebaczechu123@gmail.com>
2020-11-12 13:58:59 +0100

    Add routing to the new endpoints

--- hot_desking/priv/hot_desking.routes.erl
+++ hot_desking/priv/hot_desking.routes.erl
@@ -2,10 +2,14 @@
   security => false,
   routes => [
             {"/", { hot_desking_main_controller, index}, #{methods => [get]}}
+          , {"/choose_desk", { hot_desking_main_controller, index}, #{methods => [get]}}
 
           , {"/choose_user_and_date", { choose_user_and_date_controller, index}, #{methods => [get]}}
           , {"/all_users", { choose_user_and_date_controller, index}, #{methods => [get]}}
 
+          , {"/desks_map", { desks_map_controller, index}, #{methods => [get]}}
+          , {"/desks", { desks_map_controller, index}, #{methods => [get]}}
+          , {"/walls", { desks_map_controller, index}, #{methods => [get]}}
            ],
  statics => [
              {"/assets/[...]", "assets"}
